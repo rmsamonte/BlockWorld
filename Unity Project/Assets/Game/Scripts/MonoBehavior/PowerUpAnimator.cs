8ebe939f99db1ba7d2644b8e2e49ce0a30a38402
Vladimir <vladroot@gmail.com>
2016-04-19 20:09:29 +0300

    Changed the power up set (updated existing, added new, removed unwanted). Scale of animals and food items now more realistic. Added animation to food items.

---
+++ Unity Project/Assets/Game/Scripts/MonoBehavior/PowerUpAnimator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpAnimator : MonoBehaviour
+{
+	public bool isAnimated = false;
+
+	void Update()
+	{
+		float height = Mathf.PerlinNoise(Time.time, 0);
+
+		Vector3 pos = transform.localPosition;
+		pos.y = height;
+		transform.localPosition = pos;
+	}
+}
