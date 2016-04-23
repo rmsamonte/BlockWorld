f5f879122033bfe415b4b8608058f9bfe774ab7a
Vladimir <vladroot@gmail.com>
2016-04-23 22:59:21 +0300

    Added player model and animator for it (at least of what could be animated).

---
+++ Unity Project/Assets/Game/Scripts/MonoBehavior/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimator : MonoBehaviour
+{
+	public Animator anim;
+
+	private CharacterMotor chrMotor;
+
+	void Start()
+	{
+		chrMotor = GetComponent<CharacterMotor>();
+	}
+
+	void Update()
+	{
+		Debug.Log(chrMotor.movement.velocity);
+		anim.SetFloat("velocity",
+			Mathf.Sqrt(chrMotor.movement.velocity.z * chrMotor.movement.velocity.z + chrMotor.movement.velocity.x * chrMotor.movement.velocity.x));
+		anim.SetFloat("JumpVelocity", chrMotor.movement.velocity.y);
+	}
+}
